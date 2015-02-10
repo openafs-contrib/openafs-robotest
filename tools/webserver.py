5eb99c78091aa8ef6388a4454d030cc3f90df018
Michael Meffie <mmeffie@sinenomine.net>
2015-02-10 17:07:01 -0500

    webserver: daemonize the webserver
    Run the webserver in a background process. Write the log
    messages to syslog instead of stdout.

--- tools/webserver.py
+++ tools/webserver.py
@@ -2,25 +2,41 @@
 
 import os
 import sys
+import syslog
 import SimpleHTTPServer
 import SocketServer
+
+class MyHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def log_message(self, format, *args):
+        syslog.syslog("%s - - [%s] %s\n" % (self.client_address[0],
+                  self.log_date_time_string(), format % args))
+
+def daemonize():
+    pid = os.fork()
+    if pid < 0:
+        sys.stderr.write("Failed to fork!\n")
+    elif pid != 0:
+        # parent process
+        print "Started process id %d." % (pid)
+        sys.exit(0)
+    else:
+        # child process
+        return
 
 def main(args):
     try:
         import settings
     except ImportError:
-        print "Please do `./run setup' first."
+        print "Please do `./run.py setup' first."
         sys.exit(1)
-    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
     if not os.path.exists(settings.RF_OUTPUT):
         os.makedirs(settings.RF_OUTPUT)
     os.chdir(settings.RF_OUTPUT)
-    httpd = SocketServer.TCPServer(("", settings.WEBSERVER_PORT), Handler)
-    print "Serving at port %d, use <Ctrl-C> to stop." % settings.WEBSERVER_PORT
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
+    httpd = SocketServer.TCPServer(("", settings.WEBSERVER_PORT), MyHTTPRequestHandler)
+    syslog.openlog(ident='webserver', logoption=syslog.LOG_PID, facility=syslog.LOG_DAEMON)
+    syslog.syslog("Starting openafs-robotest webserver on port %d ..." % settings.WEBSERVER_PORT)
+    daemonize()
+    httpd.serve_forever()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
