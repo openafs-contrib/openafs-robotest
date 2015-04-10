fbd37d21547eaa53d3c62a70038ed87886e97485
Michael Meffie <mmeffie@sinenomine.net>
2015-04-09 22:16:54 -0400

    fix path in lib2html.py

--- doc/lib2html.py
+++ doc/lib2html.py
@@ -6,10 +6,10 @@ import os
 from robot.libdoc import libdoc
 
 ROOT = os.path.normpath(os.path.join(os.path.abspath(__file__), '..', '..'))
-sys.path.insert(0, os.path.join(ROOT, 'lib'))
+sys.path.insert(0, os.path.join(ROOT, 'libraries'))
 
 if __name__ == '__main__':
-    ipath = os.path.join(ROOT, 'lib', 'OpenAFSLibrary')
+    ipath = os.path.join(ROOT, 'libraries', 'OpenAFSLibrary')
     opath = os.path.join(ROOT, 'doc', 'OpenAFSLibrary.html')
     try:
         libdoc(ipath, opath)
