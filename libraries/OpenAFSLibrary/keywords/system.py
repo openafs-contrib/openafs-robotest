02075a59e0f9ec18738ba80a3b63887e69a2e7ec
Michael Meffie <mmeffie@sinenomine.net>
2015-12-12 16:39:47 -0500

    remove unused system keywords
    Remove system keywords which were used just for installation and
    cell setup. These have been moved to afsutil.

--- libraries/OpenAFSLibrary/keywords/system.py
+++ libraries/OpenAFSLibrary/keywords/system.py
@@ -36,23 +36,6 @@ def set_global_variables():
     except AttributeError:
         pass # allow to load outside of RF
 
-def get_running_programs():
-    rc,out,err = run_program("ps ax")
-    if rc != 0:
-        raise AssertionError("Failed to run 'ps', exit code='%d'" % (rc))
-    programs = set()
-    lines = out.splitlines()
-    # The first line of the ps output is a header line which shows
-    # the columns for the fields.
-    column = lines[0].index('COMMAND')
-    for line in lines[1:]:
-        cmd_line = line[column:]
-        if cmd_line[0] == '[':  # skip linux threads
-            continue
-        command = cmd_line.split()[0]
-        programs.add(os.path.basename(command))
-    return list(programs)
-
 def get_crash_count():
     count = 0
     last = ""
@@ -65,116 +48,7 @@ def get_crash_count():
     log.close()
     return (count, last)
 
-class System:
-    @staticmethod
-    def current():
-        uname = os.uname()[0]
-        if uname == "Linux":
-            return Linux()
-        elif uname == "SunOS":
-            return Solaris()
-        else:
-            raise AssertionError("Unsupported operating system: %s" % (uname))
-
-class Linux(System):
-    def get_modules(self):
-        """Return loaded kernel module names."""
-        modules = []
-        f = open("/proc/modules", "r")
-        for line in f.readlines():
-            module = line.split()[0]
-            modules.append(module)
-        f.close()
-        return modules
-
-    def unload_module(self, name):
-        """Unload the kernel module."""
-        sudo("rmmod", name)
-
-    def get_interfaces(self):
-        # Getting information about the network interfaces in a portable way
-        # requires some non-core modules; so instead just run the native
-        # command line program.
-        addrs = []
-        cmd = "ip -oneline -family inet addr show"
-        # Example output:
-        # 1: lo    inet 127.0.0.1/8 scope host lo
-        # 2: eth0    inet 172.16.50.143/24 brd 172.16.50.255 scope global eth0
-        pipe = os.popen(cmd)
-        for line in pipe.readlines():
-            s = re.search(r'inet (\d+\.\d+\.\d+\.\d+)', line)
-            if s:
-                addr = s.group(1)
-                if not addr.startswith("127."):
-                    addrs.append(addr)
-        pipe.close
-        return addrs
-
-    def configure_dynamic_linker(self, lib=None):
-        if lib:
-            sudo("/sbin/ldconfig", lib)
-        else:
-            sudo("/sbin/ldconfig")
-
-class Solaris(System):
-    def _get_kernel_modules(self):
-        """Return loaded kernel module names and ids."""
-        modules = {}
-        pipe = os.popen("/usr/sbin/modinfo -w")
-        for line in pipe.readlines():
-            if line.lstrip().startswith("Id"):
-                continue # skip header line
-            # Fields are: Id Loadaddr Size Info Rev Module Name (Desc)
-            m = re.match(r'\s*(\d+)\s+\S+\s+\S+\s+\S+\s+\d+\s+(\S+)', line)
-            if m:
-                id = m.group(1)
-                name = m.group(2)
-                modules[name] = id  # remove duplicate entries
-            else:
-                raise AssertionError("Unexpected modinfo output: %s" % (line))
-        pipe.close()
-        return modules
-
-    def get_modules(self):
-        """Return loaded kernel module names."""
-        return self._get_kernel_modules().keys()
-
-    def unload_module(self, name):
-        """Unload the kernel module."""
-        modules = self._get_kernel_modules()
-        if name in modules:
-            sudo("modunload", "-i", modules[name])
-
-    def get_interfaces(self):
-        # Getting information about the network interfaces in a portable way
-        # requires some non-core modules; so instead just run the native
-        # command line program.
-        addrs = []
-        cmd = "ipadm show-addr -p -o STATE,ADDR"
-        # Example output:
-        # ok:127.0.0.1/8
-        # ok:172.16.50.146/24
-        # ok:\:\:1/128
-        # ok:fe80\:\:20c\:29ff\:fe3d\:f60c/10
-        pipe = os.popen(cmd)
-        for line in pipe.readlines():
-            m = re.match(r'ok:(\d+\.\d+\.\d+\.\d+)', line)
-            if m:
-                addr = m.group(1)
-                if not addr.startswith("127."):
-                    addrs.append(addr)
-        pipe.close
-        return addrs
-
-    def configure_dynamic_linker(self, lib=None):
-        if lib:
-            sudo("/usr/bin/crle", "-u", "-l", lib)
-        else:
-            sudo("/usr/bin/crle", "-u")
-
-current = System.current()
 class _SystemKeywords(object):
-
     def command_should_succeed(self, cmd, msg=None):
         """Fails if command does not exit with a zero status code."""
         rc,out,err = run_program(cmd)
@@ -188,45 +62,6 @@ class _SystemKeywords(object):
         rc,out,err = run_program(cmd)
         if rc == 0:
             raise AssertionError("Command should have failed: %s" % cmd)
-
-    def sudo(self, cmd, *args):
-        """Run a command as root."""
-        sudo(cmd, *args)
-
-    def get_host_by_name(self, hostname):
-        """Return the ipv4 address of the hostname."""
-        return socket.gethostbyname(hostname)
-
-    def get_device(self, path):
-        """Return the device id of the given path as '(major,minor)'."""
-        device = os.stat(path).st_dev
-        return "(%d,%d)" % (os.major(device), os.minor(device))
-
-    def program_should_be_running(self, program):
-        """Verify the process name is running."""
-        if program not in get_running_programs():
-            raise AssertionError("Program '%s' is not running!" % (program))
-
-    def program_should_not_be_running(self, program):
-        """Verify the process name is not running."""
-        if program in get_running_programs():
-            raise AssertionError("Program '%s' is running!" % (program))
-
-    def get_modules(self):
-        """Return a list of loaded kernel module names."""
-        return current.get_modules()
-
-    def unload_module(self, name):
-        """Unload the kernel module."""
-        return current.unload_module(name)
-
-    def get_interfaces(self):
-        """Find the non-loopback IPv4 addresses of the network interfaces."""
-        return current.get_interfaces()
-
-    def configure_dynamic_linker(self, lib=None):
-        """Configure the run-time dynamic linker."""
-        return current.configure_dynamic_linker(lib)
 
     def init_crash_check(self):
         """Initialize the crash check counter."""
