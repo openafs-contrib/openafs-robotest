84716b42417c577684f130666b76fcd517cef7d8
Michael Meffie <mmeffie@sinenomine.net>
2016-01-18 13:18:48 -0500

    lib: cleanup old versions in library helper install.

--- libraries/OpenAFSLibrary/install.sh
+++ libraries/OpenAFSLibrary/install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+rm -rf dist/*
 python setup.py sdist --formats gztar --verbose || exit 1
 sdist=`ls dist/OpenAFSLibrary*.tar.gz` || exit 1
 sudo pip install "$sdist"
--- libraries/afsutil/install.sh
+++ libraries/afsutil/install.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 # remove old versions
-sudo pip uninstall -y afsutil
-git clean -f -d -x dist afsutil.egg-info
+#sudo pip uninstall -y afsutil
+#git clean -f -d -x dist afsutil.egg-info
 
+rm -rf dist/*
 python setup.py sdist --formats gztar --verbose || exit 1
+
 sdist=`ls dist/afsutil*.tar.gz` || exit 1
 sudo pip install "$sdist"
