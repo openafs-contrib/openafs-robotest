b2ed20ebe109f58b1f83d619568656942b058f77
Michael Meffie <mmeffie@sinenomine.net>
2016-07-15 10:36:53 -0400

    afsrobot: stub out unit tests

---
+++ libraries/afsrobot/test/test_web.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2014-2016 Sine Nomine Associates
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THE SOFTWARE IS PROVIDED 'AS IS' AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+import os
+import unittest
+import afsrobot.web
+
+class WebTest(unittest.TestCase):
+
+    def test_xxx(self):
+        pass
+        #self.assertIsNotNone(...)
+        #self.assertIn(..)
+        #self.assertRaises(...)
+        #self.assertEqual(...)
+        #self.assertTrue(...)
+
+if __name__ == "__main__":
+     unittest.main()
+
