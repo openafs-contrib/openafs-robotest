2ec795ed012ebeaeb3a1d33d43d2e05990ab85a9
Michael Meffie <mmeffie@sinenomine.net>
2016-09-28 20:16:22 -0400

    install: be sure to install OpenAFSLibrary changes
    Use the pip --upgrade option to install the OpenAFSLibrary even
    if the version string has not been changed. This way it it not
    necessary to bump the version string during development changes.

--- libraries/OpenAFSLibrary/install.sh
+++ libraries/OpenAFSLibrary/install.sh
@@ -3,5 +3,5 @@
 rm -rf dist/*
 python setup.py sdist --formats gztar --verbose || exit 1
 sdist=`ls dist/OpenAFSLibrary*.tar.gz` || exit 1
-sudo pip install "$sdist"
+sudo pip install "$sdist" --upgrade
 
