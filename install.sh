66048e8894e5b2e41830da502fea54464ed33f36
Michael Meffie <mmeffie@sinenomine.net>
2016-10-03 11:42:37 -0400

    install: install deps
    Automatically install external dependencies on:
    
     * debian
     * rhel/centos
     * solaris 10 and 11

--- install.sh
+++ install.sh
@@ -19,6 +19,120 @@ LOG=$ROOT/html/log
 OUTPUT=$ROOT/html/output
 DIST=$ROOT/html/dist
 
+_detect_sysname() {
+    # We might not have python yet, so we can't use python -m platform.
+    case `uname` in
+    Linux)
+        if [ -f /etc/debian_version ]; then
+            sysname="linux-debian"
+        elif [ -f /etc/centos-release ]; then
+            sysname="linux-centos"
+        elif [ -f /etc/redhat-release ]; then
+            sysname="linux-rhel"
+        else
+            sysname="linux-unknown"
+        fi
+        ;;
+    SunOS)
+        osrel=`uname -r | sed 's/^5\.//'`
+        sysname="solaris-$osrel"
+        ;;
+    *)
+        syname="unknown"
+        ;;
+    esac
+    echo "$sysname"
+}
+
+#
+# Install dependencies on Debian.
+#
+_debian_install_deps() {
+    echo "Installing python."
+    sudo -n apt-get install -q -y python || exit 1
+    echo "Installing pip."
+    sudo -n apt-get install -q -y python-pip || exit 1
+    echo "Installing argparse."
+    sudo -n apt-get install -q -y python-argparse || exit 1
+    echo "Installing robotframework."
+    sudo -n pip -q install robotframework || exit 1
+}
+
+#
+# Install dependencies on RHEL/CentOS.
+#
+_rhel_install_deps() {
+    echo "Installing epel."
+    sudo -n yum install -q -y epel-release
+    echo "Installing python."
+    sudo -n yum install -q -y python || exit 1
+    echo "Installing pip."
+    sudo -n yum install -q -y python-pip || exit 1
+    echo "Installing argparse."
+    sudo -n yum install -q -y python-argparse || exit 1
+    echo "Installing robotframework."
+    sudo -n pip -q install robotframework || exit 1
+}
+
+#
+# Install dependencies on Solaris.
+#
+_solaris_install_deps() {
+    if [ ! -x /opt/csw/bin/pkgutil ]; then
+        cat <<_EOF_ >.pkgadd
+mail=
+instance=overwrite
+partial=nocheck
+runlevel=nocheck
+idepend=nocheck
+rdepend=nocheck
+space=nocheck
+setuid=nocheck
+conflict=nocheck
+action=nocheck
+networktimeout=60
+networkretries=3
+authentication=quit
+keystore=/var/sadm/security
+proxy=
+basedir=default
+_EOF_
+        echo "Installing OpenCSW pkgutil."
+        sudo -n pkgadd -a .pkgadd -d http://get.opencsw.org/now CSWpkgutil || exit 1
+        rm .pkgadd
+    fi
+    sudo -n /opt/csw/bin/pkgutil -U || exit 1
+    echo "Installing python 2.7."
+    sudo -n /opt/csw/bin/pkgutil -y -i python27 || exit 1
+    echo "Installing pip."
+    sudo -n /opt/csw/bin/pkgutil -y -i py_pip || exit 1
+    echo "Installing argparse."
+    sudo -n /opt/csw/bin/pkgutil -y -i py_argparse || exit 1
+    echo "Installing robotframework."
+    sudo -n pip -q install robotframework || exit 1
+}
+
+#
+# Install dependencies.
+#
+install_deps() {
+    sysname=`_detect_sysname`
+    case "$sysname" in
+    linux-debian*)
+        _debian_install_deps
+        ;;
+    linux-centos*|linux-rhel*)
+        _rhel_install_deps
+        ;;
+    solaris-10*|solaris-11*)
+        _solaris_install_deps
+        ;;
+    *)
+        echo "WARNING: Unable to install deps on unknown platform: $sysname" >&2
+        ;;
+    esac
+}
+
 #
 # Create/upgrade the configuration file.
 #
@@ -121,7 +235,13 @@ install_packages() {
 # Print usage.
 #
 usage() {
-    echo "$PROGNAME: [all|dirs|doc|packages|script|repo]"
+    echo "usage: $PROGNAME <target>"
+    echo "where <target> is one of:"
+    echo "  all  - full install (default)"
+    echo "  deps - external dependencies"
+    echo "  dirs - make directories"
+    echo "  doc  - generate docs"
+    echo "  pkg  - our python packages"
 }
 
 #
@@ -135,6 +255,9 @@ case "$1" in
     -h|--help|help)
         usage
         ;;
+    deps)
+        install_deps
+        ;;
     dirs)
         make_dirs
         ;;
@@ -145,11 +268,12 @@ case "$1" in
         make_sdists
         update_repo
         ;;
-    packages)
+    pkg)
         make_sdists
         install_packages
         ;;
     ""|all)
+        install_deps
         make_dirs
         make_doc
         make_sdists
