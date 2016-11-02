42745fab97b2383b9357f2984adddce981acf807
Michael Meffie <mmeffie@sinenomine.net>
2016-11-01 23:23:24 -0400

    install: prefix shell function names

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-PROGNAME=`basename $0`
-TARGETS=""
-VERBOSE="no"
 PACKAGES="afsutil afsrobot OpenAFSLibrary"
+OPT_VERBOSE="no"
 DO_CHECK="no"
 
 DIR_PREFIX="/usr/local"
@@ -13,8 +11,9 @@ DIR_DOC="$DIR_HTML/doc"
 DIR_LOG="$DIR_HTML/log"
 DIR_OUTPUT="$DIR_HTML/output"
 
-usage() {
-    echo "usage: sudo ./$PROGNAME [--verbose] [<target>]"
+art_usage() {
+    _progname=`basename $0`
+    echo "usage: sudo ./$_progname [--verbose] [<target>]"
     echo ""
     echo "where <target> is one of:"
     echo "  all   - full install (default)"
@@ -24,66 +23,65 @@ usage() {
     echo "  pkg   - python packages"
 }
 
-_run() {
-    if [ "$VERBOSE" = "yes" ]; then
+art_run() {
+    if [ "$OPT_VERBOSE" = "yes" ]; then
         $@ || exit 1
     else
         $@ >/dev/null || exit 1
     fi
 }
 
-_detect_sysname() {
+art_detect_sysname() {
     # We might not have python yet, so we can't use python -m platform.
     case `uname` in
     Linux)
         if [ -f /etc/debian_version ]; then
-            sysname="linux-debian"
+            echo "linux-debian"
         elif [ -f /etc/centos-release ]; then
-            sysname="linux-centos"
+            echo "linux-centos"
         elif [ -f /etc/redhat-release ]; then
-            sysname="linux-rhel"
+            echo "linux-rhel"
         else
-            sysname="linux-unknown"
+            echo "linux-unknown"
         fi
         ;;
     SunOS)
-        osrel=`uname -r | sed 's/^5\.//'`
-        sysname="solaris-$osrel"
+        _osrel=`uname -r | sed 's/^5\.//'`
+        echo "solaris-$_osrel"
         ;;
     *)
-        syname="unknown"
+        echo "unknown"
         ;;
     esac
-    echo "$sysname"
 }
 
-_debian_install_deps() {
+art_debian_install_deps() {
     # Install dependencies on Debian.
     echo "Installing python."
-    _run apt-get install -q -y python
+    art_run apt-get install -q -y python
     echo "Installing pip."
-    _run apt-get install -q -y python-pip
+    art_run apt-get install -q -y python-pip
     echo "Installing argparse."
-    _run apt-get install -q -y python-argparse
+    art_run apt-get install -q -y python-argparse
     echo "Installing robotframework."
-    _run pip -q install robotframework
+    art_run pip -q install robotframework
 }
 
-_rhel_install_deps() {
+art_rhel_install_deps() {
     # Install dependencies on RHEL/CentOS.
     echo "Installing epel."
-    _run yum install -q -y epel-release
+    art_run yum install -q -y epel-release
     echo "Installing python."
-    _run yum install -q -y python
+    art_run yum install -q -y python
     echo "Installing pip."
-    _run yum install -q -y python-pip
+    art_run yum install -q -y python-pip
     echo "Installing argparse."
-    _run yum install -q -y python-argparse
+    art_run yum install -q -y python-argparse
     echo "Installing robotframework."
-    _run pip -q install robotframework
+    art_run pip -q install robotframework
 }
 
-_solaris_install_deps() {
+art_solaris_install_deps() {
     # Install dependencies on Solaris.
     if [ ! -x /opt/csw/bin/pkgutil ]; then
         cat <<_EOF_ >.pkgadd
@@ -105,31 +103,31 @@ proxy=
 basedir=default
 _EOF_
         echo "Installing OpenCSW pkgutil."
-        _run pkgadd -a .pkgadd -d http://get.opencsw.org/now CSWpkgutil
+        art_run pkgadd -a .pkgadd -d http://get.opencsw.org/now CSWpkgutil
         rm -f .pkgadd
     fi
-    _run /opt/csw/bin/pkgutil -U
+    art_run /opt/csw/bin/pkgutil -U
     echo "Installing python 2.7."
-    _run /opt/csw/bin/pkgutil -y -i python27
+    art_run /opt/csw/bin/pkgutil -y -i python27
     echo "Installing pip."
-    _run /opt/csw/bin/pkgutil -y -i py_pip
+    art_run /opt/csw/bin/pkgutil -y -i py_pip
     echo "Installing argparse."
-    _run /opt/csw/bin/pkgutil -y -i py_argparse
+    art_run /opt/csw/bin/pkgutil -y -i py_argparse
     echo "Installing robotframework."
-    _run pip -q install robotframework
+    art_run pip -q install robotframework
 }
 
-install_deps() {
-    sysname=`_detect_sysname`
+art_install_deps() {
+    sysname=`art_detect_sysname`
     case "$sysname" in
     linux-debian*)
-        _debian_install_deps
+        art_debian_install_deps
         ;;
     linux-centos*|linux-rhel*)
-        _rhel_install_deps
+        art_rhel_install_deps
         ;;
     solaris-10*|solaris-11*)
-        _solaris_install_deps
+        art_solaris_install_deps
         ;;
     *)
         echo "WARNING: Unable to install deps on unknown platform: $sysname" >&2
@@ -137,79 +135,78 @@ install_deps() {
     esac
 }
 
-make_output_dirs() {
-    _run mkdir -p $DIR_LOG
-    _run mkdir -p $DIR_OUTPUT
+art_make_output_dirs() {
+    art_run mkdir -p $DIR_LOG
+    art_run mkdir -p $DIR_OUTPUT
 }
 
-make_doc() {
+art_make_doc() {
     # Generate the library documentation. Requires robotframework.
     pypath=libraries/OpenAFSLibrary/OpenAFSLibrary
     input="$pypath"
     output="$DIR_DOC/OpenAFSLibary.html"
     echo "Generating documentation file $output."
-    _run mkdir -p $DIR_DOC
-    _run python -m robot.libdoc --format HTML --pythonpath $pypath $input $DIR_DOC/OpenAFSLibary.html
+    art_run mkdir -p $DIR_DOC
+    art_run python -m robot.libdoc --format HTML --pythonpath $pypath $input $DIR_DOC/OpenAFSLibary.html
 }
 
-install_package() {
-    package=$1
-    ( cd libraries/$package && ./install.sh ) || exit 1
+art_install_package() {
+    ( cd libraries/$1 && ./install.sh )
 }
 
-install_packages() {
+art_install_packages() {
     for package in $PACKAGES
     do
         echo "Installing package ${package}."
-        _run install_package $package
+        art_run art_install_package $package
     done
 }
 
-install_tests() {
-    _run mkdir -p $DIR_ROOT
-    _run cp -r tests/ $DIR_ROOT
-    _run cp -r resources/ $DIR_ROOT
+art_install_tests() {
+    art_run mkdir -p $DIR_ROOT
+    art_run cp -r tests/ $DIR_ROOT
+    art_run cp -r resources/ $DIR_ROOT
 }
 
 while :; do
     case "$1" in
     -h|--help|help)
-        usage
+        art_usage
         break
         ;;
     -v|--verbose)
-        VERBOSE="yes"
+        OPT_VERBOSE="yes"
         shift
         ;;
     deps)
-        install_deps
+        art_install_deps
         shift
         ;;
     doc)
-        make_doc
+        art_make_doc
         break
         ;;
     pkg)
-        install_packages
+        art_install_packages
         DO_CHECK="yes"
         break
         ;;
     tests)
-        install_tests
-        make_output_dirs
+        art_install_tests
+        art_make_output_dirs
         break
         ;;
     all|"")
-        install_deps
-        install_packages
-        install_tests
-        make_output_dirs
-        make_doc
+        art_install_deps
+        art_install_packages
+        art_install_tests
+        art_make_output_dirs
+        art_make_doc
         DO_CHECK="yes"
         break
         ;;
     *)
-        usage
+        art_usage
         exit 1
         ;;
     esac
