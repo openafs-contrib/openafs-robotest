560867046da85cc8eea8c2bab951a319895f307c
Michael Meffie <mmeffie@solc.(none)>
2017-02-09 03:06:19 -0500

    add solaris 11 support to afsutil getdeps
    Install solaris studio and needed developer packages on solaris 11.x.
    
    The user must generate and download the creds from the Oracle developers site
    before running afutil getdeps. The key and cert files can resided on a private
    url to make it easier to setup build machines (for example behind a firewall).
    
        afsutil getdeps --creds http://example.com/mycreds

--- libraries/afsutil/afsutil/build.py
+++ libraries/afsutil/afsutil/build.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2014-2016 Sine Nomine Associates
+# Copyright (c) 2014-2017 Sine Nomine Associates
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -26,6 +26,9 @@ import sys
 import re
 import shlex
 import platform
+import urllib2
+import tempfile
+import pprint
 
 from afsutil.system import sh, CommandFailed, file_should_exist, tar
 import afsutil.service
@@ -176,6 +179,125 @@ def _fedora_getdeps():
        'rpm-build',
        'redhat-rpm-config')
 
+
+def _download(baseurl, filename, path):
+    url = os.path.join(baseurl, filename)
+    dst = os.path.join(path, filename)
+    rsp = urllib2.urlopen(url)
+    logger.info("Downloading %s to %s", url, dst)
+    with open(dst, 'wb') as fh:
+        fh.write(rsp.read())
+
+def _compare_versions(a, b):
+    def _normalize(s):
+        return [int(x) for x in s.split('.')]
+    return cmp(_normalize(a), _normalize(b))
+
+def _sol11_getdeps(**kwargs):
+    """Install solaris studio and development packages. 
+
+    Before running this function, create an account on the Oracle Technology Network
+    and follow the instructions to create and download the key and certificate files.
+    Place the key and cert file in a local directory or at a private url.  The
+    default location of the key and cert files is /root/creds. Set the 'creds'
+    keyward argurment to specify a different path or a url.
+    """
+    creds = kwargs.get('creds', '/root/creds')
+    key = kwargs.get('key', 'pkg.oracle.com.key.pem')
+    cert = kwargs.get('cert', 'pkg.oracle.com.certificate.pem')
+    path = None
+    tmpdir = None
+
+    try:
+        if creds.startswith('http://') or creds.startswith('https://'):
+            path = tmpdir = tempfile.mkdtemp()
+            _download(creds, key, tmpdir)
+            _download(creds, cert, tmpdir)
+        elif os.path.exists(creds):
+            path = creds
+        else:
+            raise ValueError("creds path '%s' not found." % creds)
+
+        logger.info("Setting publisher for solarisstudio.")
+        sh('pkg', 'set-publisher',   # Will refresh if already set.
+       	   '-k', os.path.join(path, key),
+       	   '-c', os.path.join(path, cert),
+       	   '-G', '*',
+           '-g', 'https://pkg.oracle.com/solarisstudio/release', 'solarisstudio')
+
+        logger.info("Getting available solarisstudio packages.")
+        output = sh('pkg', 'list', '-H', '-a', '-v', '--no-refresh', 'pkg://solarisstudio/*', output=True, quiet=True)
+        packages = {}
+        installed = False # alread installed? 
+        for line in output:
+            # Extract the root package name, version, and install state.
+            # Example:
+            # 'pkg://solarisstudio/developer/developerstudio-125@12.5,5.11-1.0.0.0:20160614T214517Z ---'
+            # name='developerstudio-125'; version='12.5'; installed=False
+            fmri,ifo = line.split()
+            pkg,vt = fmri.split('@')
+            v,t = vt.split(':')
+            version = v.split(',')[0]
+            name = pkg.replace('pkg://solarisstudio/developer/','')
+            istate = ifo[0] == 'i'
+            if not '/' in name:
+                # skip non-root ones, e.g. 'developerstudio-125/cc'
+                logger.info("Found package %s, version %s, installed %s" % (name, version, istate))
+                packages[name] = {'version':version, 'installed':istate}
+                if istate:
+                    installed = True
+
+        if installed:
+            logger.info("Skipping solarisstudio install; already installed.")
+        else:
+            logger.info("Determining which solarisstudio package to install.") 
+            pkg = None
+            vers = '0.0' # Find the most recent version.
+            for name in packages.keys():
+                if _compare_versions(packages[name]['version'], vers) > 0:
+                    vers = packages[name]['version']
+                    pkg = 'pkg://solarisstudio/developer/%s' % name
+            if pkg is None:
+                raise AssertionError("Unable to find a solarisstudio package to install.") 
+
+            logger.info("Installing solarisstudio package '%s'." % (pkg)) 
+            sh('pkg', 'install', '--accept', pkg)
+
+
+        logger.info("Installing development packages.")
+        try:
+            sh('pkg', 'install',
+            'autoconf',
+            'automake',
+            'bison',
+            'flex',
+            'git',
+            'gnu-binutils',
+            'gnu-coreutils',
+            'gnu-sed',
+            'libtool',
+            'make',
+            'onbld',
+            'text/locale',
+            )
+        except CommandFailed as e:
+            if e.code != 4:  # 4 is means installed (not an error)
+                logger.error("pkg install failed: %s" % e)
+    
+    except urllib2.HTTPError as e:
+        logger.error("Unable to download files from url '%s', %s'" % (creds, e)) 
+    except ValueError as e:
+        logger.error("%s" % e)
+    finally:
+        if tmpdir:
+            logger.info("Cleaning up temp files.")
+            if os.path.exists(os.path.join(tmpdir, key)):
+                os.remove(os.path.join(tmpdir, key)) 
+            if os.path.exists(os.path.join(tmpdir, cert)):
+                os.remove(os.path.join(tmpdir, cert))
+            if os.path.exists(tmpdir) and tmpdir != "/":
+                os.rmdir(tmpdir)
+
 def getdeps(**kwargs):
     """Install build dependencies for this platform."""
     system = platform.system()
@@ -190,7 +312,11 @@ def getdeps(**kwargs):
         else:
             raise AssertionError("Unsupported dist: %s" % (dist))
     elif system == 'SunOS':
-        raise NotImplementedError()
+        release = platform.release()
+        if release == '5.11':
+            _sol11_getdeps(**kwargs)
+        else:
+            raise NotImplementedError()
     else:
         raise AssertionError("Unsupported operating system: %s" % (system))
 
