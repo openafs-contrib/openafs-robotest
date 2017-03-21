3ab6efb0187485d100fecf897e86e5d45704dc49
Michael Meffie <mmeffie@sinenomine.net>
2017-03-21 10:19:01 -0400

    os independent install.sh
    Since we now have platform dependent scripts to handle installing preqs
    on various platforms, simplify the install script to make it platform
    independent.

--- install.sh
+++ install.sh
@@ -1,385 +1,158 @@
 #!/bin/bash
+# Copyright (c) 2014-2017, Sine Nomine Associates
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THE SOFTWARE IS PROVIDED 'AS IS' AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
 
-PACKAGES="afsutil afsrobot OpenAFSLibrary"
+OPT_QUIET="no"
 OPT_VERBOSE="no"
-OPT_ALLOW_USER="no"
+OPT_PREFIX="/usr/local"
+OPT_INSTALL_LIBS="no"
+OPT_INSTALL_TESTS="no"
+OPT_INSTALL_DOCS="no"
 
-DIR_PREFIX="/usr/local"
-DIR_ROOT="$DIR_PREFIX/afsrobotest"
+# Turn off annoying pip version warnings.
+PIP_DISABLE_PIP_VERSION_CHECK=1
+
+die() {
+    echo "$1" >&2
+    exit 1
+}
+
+info() {
+    if [ $OPT_QUIET = "no" ]; then
+        echo "$1"
+    fi
+}
+
+run() {
+    if [ "$OPT_VERBOSE" = "yes" ]; then
+        $@ || die "Command failed: $@"
+    else
+        $@ >/dev/null || die "Command failed: $@"
+    fi
+}
+
+install_library() {
+    info "Installing $1"
+    if [ "$OPT_VERBOSE" = "yes" ]; then
+        (cd libraries/$1 && ./install.sh)
+    else
+        (cd libraries/$1 && ./install.sh >/dev/null)
+    fi
+}
+
+gopt() {
+    if [ -z "$2" ]; then
+        echo "Option --$1 requires an argument." >&2
+        exit 1
+    fi
+    echo "$2"
+}
+
+usage() {
+    cat <<EOF
+usage: sudo ./install.sh [--help] [--quiet] [--verbose] [--prefix <path>] [<component> [...]]
+
+where:
+   --help        display help then exit
+   --quiet       no progress messages
+   --verbose     more verbose output
+   --prefix      installation path (default: /usr/local)
+   <component>   valid components: libs, tests, docs (default: all)
+EOF
+}
+
+while :; do
+    case "$1" in
+    -h|--help)    usage; exit 0;;
+    -v|--verbose) OPT_VERBOSE="yes"; shift;;
+    -q|--quiet)   OPT_QUIET="yes"; shift;;
+    -p|--prefix)  OPT_PREFIX=`gopt prefix "$2"`; shift 2;;
+    --)           break;;
+    -?*)          echo "Unknown option: $1"; usage; exit 1;;
+    *)            break;;
+    esac
+done
+if [ $# -eq 0 ]; then
+    OPT_INSTALL_LIBS="yes"
+    OPT_INSTALL_TESTS="yes"
+    OPT_INSTALL_DOCS="yes"
+else
+    for arg; do
+        case "$arg" in
+        lib|libs)   OPT_INSTALL_LIBS="yes" ;;
+        test|tests) OPT_INSTALL_TESTS="yes" ;;
+        doc|docs)   OPT_INSTALL_DOCS="yes" ;;
+        *)          echo "Invalid component name: $arg" >&2; usage; exit 1;;
+        esac
+    done
+fi
+
+# Installation directories.
+DIR_ROOT="$OPT_PREFIX/afsrobotest"
 DIR_HTML="$DIR_ROOT/html"
 DIR_DOC="$DIR_HTML/doc"
 DIR_LOG="$DIR_HTML/log"
 DIR_OUTPUT="$DIR_HTML/output"
 
-# Turn off annoying pip version warnings.
-PIP_DISABLE_PIP_VERSION_CHECK=1
+# Get paths to python and pip. Use the OpenCSW version if installed.
+PYTHON=`PATH=/opt/csw/bin:$PATH which python`
+PIP=`PATH=/opt/csw/bin:$PATH which pip`
 
-usage() {
-    _progname=`basename $0`
-    echo "usage: ./$_progname [--user] [--verbose] [<target> ...]"
-    echo ""
-    echo "where <target> is:"
-    echo "  all   - full install (default)"
-    echo "  deps  - external dependencies"
-    echo "  tests - test suites"
-    echo "  docs  - generate docs"
-    echo "  libs  - libraries"
-}
+# Pre install check.
+if [ $UID -ne 0 ]; then
+    die "Please run as root."
+fi
+test -d $OPT_PREFIX || die "--prefix $OPT_PREFIX does not exist."
+test -x $PYTHON || die "python not found in PATH."
+if [ $OPT_INSTALL_LIBS = "yes" ]; then
+    test -x $PIP || die "pip not found in PATH."
+fi
 
-run() {
-    if [ "$OPT_VERBOSE" = "yes" ]; then
-        $@ || exit 1
-    else
-        $@ >/dev/null || exit 1
-    fi
-}
-
-detect_sysname() {
-    # We might not have python yet, so we can't use python -m platform.
-    local sname=`uname -s`
-    local rname=`uname -r`
-    local vname=`uname -v`
-    case $sname in
-    Linux)
-        if [ -f /etc/debian_version ]; then
-            echo "linux-debian"
-        elif [ -f /etc/fedora-release ]; then
-            echo "linux-fedora"
-        elif [ -f /etc/centos-release ]; then
-            echo "linux-centos"
-        elif [ -f /etc/redhat-release ]; then
-            echo "linux-rhel"
-        else
-            echo "unknown"
-        fi
-        ;;
-    SunOS)
-        case $rname in
-        5.10)
-            echo "solaris-10"
-            ;;
-        5.11)
-            echo "solaris-$vname"
-            ;;
-        *)
-            echo "unknown"
-            ;;
-        esac
-        ;;
-    *)
-        echo "unknown"
-        ;;
-    esac
-}
-
-debian_install_deps() {
-    # Install missing dependencies on Debian.
-    if ! dpkg --status python >/dev/null 2>/dev/null; then
-        echo "Installing python."
-        run apt-get install -q -y python
-    fi
-    if ! dpkg --status python-pip >/dev/null 2>/dev/null; then
-        echo "Installing pip."
-        run apt-get install -q -y python-pip
-    fi
-    # argparse has been pulled into the python core package on
-    # modern systems, but it a separate package on older systems.
-    if ! python -c 'import argparse' >/dev/null 2>/dev/null; then
-        echo "Installing argparse."
-        run apt-get install -q -y python-argparse
-    fi
-    if ! python -c 'import robot.api' >/dev/null 2>/dev/null; then
-        echo "Installing robotframework."
-        run pip -q install robotframework
-    fi
-}
-
-fedora_install_deps() {
-    # Install missing dependencies on Fedora.
-    if ! rpm -q python >/dev/null 2>/dev/null; then
-        echo "Installing python."
-        run dnf install -q -y python
-    fi
-    if ! rpm -q python-pip >/dev/null 2>/dev/null; then
-        echo "Installing pip."
-        run dnf install -q -y python-pip
-    fi
-    if ! python -c 'import robot.api' >/dev/null 2>/dev/null; then
-        echo "Installing robotframework."
-        run pip -q install robotframework
-    fi
-}
-
-rhel_install_deps() {
-    # Install missing dependencies on RHEL/CentOS.
-    if ! rpm -q epel-release >/dev/null 2>/dev/null; then
-        echo "Installing epel."
-        run yum install -q -y epel-release
-    fi
-    if ! rpm -q python >/dev/null 2>/dev/null; then
-        echo "Installing python."
-        run yum install -q -y python
-    fi
-    if ! rpm -q python-pip >/dev/null 2>/dev/null; then
-        echo "Installing pip."
-        run yum install -q -y python-pip
-    fi
-    # argparse has been pulled into the python core package on
-    # modern systems, but it a separate package on older systems.
-    if ! python -c 'import argparse' >/dev/null 2>/dev/null; then
-        echo "Installing argparse."
-        run yum install -q -y python-argparse
-    fi
-    if ! python -c 'import robot.api' >/dev/null 2>/dev/null; then
-        echo "Installing robotframework."
-        run pip -q install robotframework
-    fi
-}
-
-solaris10_install_deps() {
-    # Install dependencies on Solaris.
-    DID_UPDATE='no'
-    if [ ! -x /opt/csw/bin/pkgutil ]; then
-        cat <<_EOF_ >/tmp/pkgadd-conf-$$
-mail=
-instance=overwrite
-partial=nocheck
-runlevel=nocheck
-idepend=nocheck
-rdepend=nocheck
-space=nocheck
-setuid=nocheck
-conflict=nocheck
-action=nocheck
-networktimeout=60
-networkretries=3
-authentication=quit
-keystore=/var/sadm/security
-proxy=
-basedir=default
-_EOF_
-        echo "Installing OpenCSW pkgutil."
-        run pkgadd -a /tmp/pkgadd-conf-$$ -d http://get.opencsw.org/now CSWpkgutil
-        rm -f /tmp/pkgadd-conf-$$
-    fi
-    if ! /opt/csw/bin/pkgutil --list | grep '^CSWpython27$' >/dev/null; then
-        echo "Installing python 2.7."
-        if [ $DID_UPDATE = 'no' ]; then
-            run /opt/csw/bin/pkgutil -U
-            DID_UPDATE='yes'
-        fi
-        run /opt/csw/bin/pkgutil -y -i python27
-    fi
-    if ! /opt/csw/bin/pkgutil --list | grep '^CSWpy-pip$' >/dev/null; then
-        echo "Installing pip."
-        if [ $DID_UPDATE = 'no' ]; then
-            run /opt/csw/bin/pkgutil -U
-            DID_UPDATE='yes'
-        fi
-        run /opt/csw/bin/pkgutil -y -i py_pip
-    fi
-    if ! /opt/csw/bin/pkgutil --list | grep '^CSWpy-argparse$' >/dev/null; then
-        echo "Installing argparse."
-        if [ $DID_UPDATE = 'no' ]; then
-            run /opt/csw/bin/pkgutil -U
-            DID_UPDATE='yes'
-        fi
-        run /opt/csw/bin/pkgutil -y -i py_argparse
-    fi
-    if ! /opt/csw/bin/python -c 'import robot.api' >/dev/null 2>/dev/null; then
-        echo "Installing robotframework."
-        run /opt/csw/bin/pip -q install robotframework
-    fi
-}
-
-solaris11_install_deps() {
-    if pkg verify -q pkg://solaris/runtime/python-27 2>/dev/null; then
-        :
-    else
-        echo "Installing python 2.7."
-        run pkg install pkg://solaris/runtime/python-27
-    fi
-    if pkg verify -q pkg://solaris/library/python/pip 2>/dev/null; then
-        :
-    else
-        echo "Installing pip."
-        run pkg install pkg://solaris/library/python/pip
-    fi
-    # argparse is bundled in runtime/python-27
-    if ! python -c 'import robot.api' >/dev/null 2>/dev/null; then
-        echo "Installing robotframework."
-        run pip -q install robotframework
-    fi
-}
-
-# The nm changed in 11.3 which broke libtool.
-# See http://lists.gnu.org/archive/html/bug-libtool/2016-01/msg00000.html
-solaris113_patch_libtool() {
-    if [ -f /usr/share/aclocal/libtool.m4.unpatched ]; then
-        echo "Skipping libtool patch; already patched."
-        return
-    fi
-    cp /usr/share/aclocal/libtool.m4 /usr/share/aclocal/libtool.m4.unpatched
-    chmod +w /usr/share/aclocal/libtool.m4
-    echo "Patching libtool"
-    ( cd / ; cat <<_EOF_ | patch -p1 )
---- /usr/share/aclocal/libtool.m4.orig	Sun Aug 14 12:04:03 2016
-+++ /usr/share/aclocal/libtool.m4	Sun Aug 14 12:05:55 2016
-@@ -3651,7 +3651,7 @@
-   symcode='[[BCDEGQRST]]'
-   ;;
- solaris*)
--  symcode='[[BDRT]]'
-+  symcode='[[BCDRT]]'
-   ;;
- sco3.2v5*)
-   symcode='[[DT]]'
-_EOF_
-}
-
-install_deps() {
-    echo "Checking dependencies."
-    sysname=`detect_sysname`
-    case "$sysname" in
-    linux-debian*)
-        debian_install_deps
-        ;;
-    linux-fedora*)
-        fedora_install_deps
-        ;;
-    linux-centos*|linux-rhel*)
-        rhel_install_deps
-        ;;
-    solaris-10*)
-        solaris10_install_deps
-        ;;
-    solaris-11.[012])
-        # Install OpenCSW packages on old versions.
-        solaris10_install_deps
-        ;;
-    solaris-11.3)
-        solaris11_install_deps
-        solaris113_patch_libtool
-        ;;
-    *)
-        echo "WARNING: Unable to install deps on unknown platform: $sysname" >&2
-        ;;
-    esac
-}
-
-make_output_dirs() {
-    echo "Making output directories."
-    run mkdir -p $DIR_LOG
-    run mkdir -p $DIR_OUTPUT
-}
-
-make_doc() {
-    echo "Generating documentation."
-    # Generate the library documentation. Requires robotframework.
-    pypath=libraries/OpenAFSLibrary/OpenAFSLibrary
-    input="$pypath"
-    output="$DIR_DOC/OpenAFSLibary.html"
-    if [ "$OPT_VERBOSE" = "yes" ]; then
-        echo "Writing file $output"
-    fi
-    run mkdir -p $DIR_DOC
-
-    # Get a useable python on old solaris.
-    PYTHON=`PATH=/opt/csw/bin:$PATH which python`
-    if [ $? -ne 0 ]; then
-        echo "Cannot generate docs; python is not in the PATH"
-    else
-        run $PYTHON -m robot.libdoc --format HTML --pythonpath $pypath $input $DIR_DOC/OpenAFSLibary.html
-    fi
-}
-
-install_package() {
-    ( cd libraries/$1 && ./install.sh )
-}
-
-install_packages() {
-    echo "Installing our packages."
-    for package in $PACKAGES
-    do
-        echo "  Installing package ${package}."
-        run install_package $package
-    done
-}
-
-install_tests() {
-    echo "Installing test suites."
+# Install.
+if [ $OPT_INSTALL_LIBS = "yes" ]; then
+    install_library afsutil
+    install_library afsrobot
+    install_library OpenAFSLibrary
+fi
+if [ $OPT_INSTALL_TESTS = "yes" ]; then
+    info "Installing test suites"
     run mkdir -p $DIR_ROOT
     run cp -r tests/ $DIR_ROOT
     run cp -r resources/ $DIR_ROOT
-}
-
-SEEN=""
-while :; do
-    case "$1" in
-    "")
-        break
-        ;;
-    -h|--help)
-        usage
-        exit 0
-        ;;
-    -u|--user)
-        OPT_ALLOW_USER="yes"
-        shift
-        ;;
-    -v|--verbose)
-        OPT_VERBOSE="yes"
-        shift
-        ;;
-    deps|docs|libs|tests)
-        SEEN="$SEEN:$1:"
-        shift
-        ;;
-    all)
-        SEEN="$SEEN:deps:docs:libs:tests:"
-        shift
-        ;;
-    *)
-        usage
-        exit 1
-        ;;
-    esac
-done
-
-# Avoid accidental local installs by requiring --user when not running as root.
-if [ $UID -ne 0 -a "$OPT_ALLOW_USER" = "no" ]; then
-    echo "Please run as root, or specify --user for a local installation."
-    exit 1
+    info "Making output directories"
+    run mkdir -p $DIR_LOG
+    run mkdir -p $DIR_OUTPUT
 fi
-if [ $UID -eq 0 -a "$OPT_ALLOW_USER" = "yes" ]; then
-    echo "Cannot do a local installation as root."
-    exit 1
+if [ $OPT_INSTALL_DOCS = "yes" ]; then
+    info "Generating documentation"
+    pypath=libraries/OpenAFSLibrary/OpenAFSLibrary
+    input="$pypath"
+    output="$DIR_DOC/OpenAFSLibary.html"
+    run mkdir -p $DIR_DOC
+    run $PYTHON -m robot.libdoc --format HTML --pythonpath $pypath $input $DIR_DOC/OpenAFSLibary.html
 fi
 
-# Install specified targets.
-if [ -z $SEEN ]; then
-    SEEN=":deps:docs:libs:tests:"  # Install all by default.
+# Post install check.
+if [ $OPT_INSTALL_LIBS = "yes" ]; then
+    afsutil check || echo "Try: sudo afsutil check --fix-hosts"
 fi
-for TARGET in "deps" "libs" "tests" "docs"
-do
-    if echo "$SEEN" | grep ":$TARGET:" >/dev/null; then
-        case "$TARGET" in
-        deps)    install_deps ;;
-        libs)    install_packages ;;
-        tests)   install_tests ;;
-        docs)    make_doc ;;
-        esac
-    fi
-done
 
-# Post install steps.
-if echo "$SEEN" | grep ":tests:" >/dev/null; then
-    make_output_dirs
-fi
-if echo "$SEEN" | grep ":libs:" >/dev/null; then
-    afsutil check
-    if [ $? -ne 0 ]; then
-        echo "Try: sudo afsutil check --fix-hosts"
-    fi
-fi
-echo "Done."
+info "Done"
