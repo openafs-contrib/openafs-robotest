075962d84cc994c90928da5331bbe2a74f2033b4
Michael Meffie <mmeffie@sinenomine.net>
2017-09-20 15:05:37 -0400

    afsrobot: example shell script

---
+++ libraries/afsrobot/examples/run-tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+set -x
+
+rm -f ~/afsrobot/afsrobot.ini
+afsrobot init --quiet
+afsrobot config set test aklog /usr/local/bin/aklog-1.6
+afsrobot config set host.0 installer transarc
+afsrobot config set host.0 dest ~/src/openafs/amd64_linux26/dest
+afsrobot config set host.1 name deb9a
+afsrobot config set host.1 installer transarc
+afsrobot config set host.1 dest ~/src/openafs/amd64_linux26/dest
+afsrobot config set host.1 dafileserver "-d 10"
+afsrobot config set cell fs mantis,deb9a
+
+afsrobot setup
+afsrobot test
+afsrobot teardown
