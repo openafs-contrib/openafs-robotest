1f3330f89247fc7ca8029aaad6a682adbe960abc
Michael Meffie <mmeffie@sinenomine.net>
2019-03-26 08:14:28 -0400

    Change example config format from yaml to py
    YAML variable files require the PyYAML module, which needs to be
    installed. So just use regular python variable files to remove this
    dependency.

---
+++ examples/settings.py
@@ -0,0 +1,15 @@
+# Realm'
+KRB_REALM = 'EXAMPLE.COM'
+KRB_ADMIN_KEYTAB = '/tmp/afsrobot/user.keytab'
+KRB_AFS_KEYTAB = '/tmp/afsrobot/fake.keytab'
+KRB_USER_KEYTAB = '/tmp/afsrobot/user.keytab'
+
+# Cell
+AFS_CELL = 'example.com'
+AFS_USER = 'afsrobot'
+AFS_ADMIN = 'afsrobot.admin'
+AFS_AKIMPERSONATE = 'true'
+AFS_FILESERVERS = 'bismark,'
+
+# Client options
+PAG_ONEGROUP = True
