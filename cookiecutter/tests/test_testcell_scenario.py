cf82355a8cd8af6c669d6632efe15d65799e6d74
Michael Meffie <mmeffie@sinenomine.net>
2022-06-24 16:27:59 -0400

    cookiecutter: Update cookiecutter unit tests

--- cookiecutter/tests/test_testcell_scenario.py
+++ cookiecutter/tests/test_testcell_scenario.py
@@ -1,4 +1,5 @@
-import collections
+import json
+import itertools
 import os
 import pathlib
 import pytest
@@ -6,17 +7,32 @@ import pytest
 from cookiecutter.main import cookiecutter
 from contextlib import contextmanager
 
-platforms = [
-    'alma8',
-    'debian11',
-    'fedora35',
-    'solaris114',
-]
+DRIVER = os.environ.get('MOLECULE_DRIVER_NAME', 'vagrant/libvirt')
 
-install_methods = ['managed', 'packages', 'bdist', 'sdist', 'source']
-layout = [
-    '1 host',
-]
+cluster_types = {
+    '1host': "one instance",
+    '2hosts': "one client instance, one server instance",
+    '9hosts': "three client instances, three database instances, three fileserver instances",
+}
+
+
+def is_valid(mode, row):
+    d, c, p, i = row
+    if mode == 'run' and d != DRIVER:
+        return False
+    if p.startswith('solaris') and i in ('managed', 'packages'):
+        return False
+    return True
+
+
+def params(mode):
+    cookiecutter_json = pathlib.Path(__file__).parent.parent / 'testcell-scenario' / 'cookiecutter.json'
+    with open(cookiecutter_json) as f:
+        c = json.load(f)
+    for row in itertools.product(c['driver'], cluster_types.keys(), c['platform'], c['install_method']):
+        if is_valid(mode, row):
+            yield(row)
+
 
 @contextmanager
 def chdir(path):
@@ -27,47 +43,18 @@ def chdir(path):
     finally:
         os.chdir(prev)
 
-def params():
-    p = []
-    for x in platforms:
-        for y in install_methods:
-            if x.startswith('solaris') and y in ['managed', 'packages']:
-                continue  # package installs not supported on solaris.
-            p.append((x, y, 0))
-    for hi, _ in enumerate(layout):
-        if hi != 0:
-            p.append((platforms[0], install_methods[0], hi))
-    return p
 
 def run_command(c):
     print('Running:', c)
     return os.system(c)
 
-def get_str_option(name, default):
-    return os.environ.get(name, default)
 
-def get_bool_option(name, default):
-    value = get_str_option(name, default)
-    if value.lower() in ('1', 'y', 'yes', 'true', 'on'):
-        return True
-    if value.lower() in ('0', 'n', 'no', 'false', 'off'):
-        return False
-    raise ValueError('Invalid value %s: %s' % (name, value))
-
-@pytest.mark.parametrize('platform,install_method,layout_index', params())
-def test_template(tmpdir, install_method, platform, layout_index):
-
-    print("\nRunning in directory %s" % tmpdir)
-
-    # Test options.
-    lint = get_bool_option('TCS_LINT', 'yes')
-    run = get_bool_option('TCS_RUN', 'no')
-    install_with_dkms = get_bool_option('TCS_ENABLE_DKMS', 'no')
-    build_packages = get_bool_option('TCS_ENABLE_BUILDS', 'yes')
-    do_build = (build_packages and install_method in ('packages', 'bdist', 'sdist'))
-
-    # Render the template in the tmpdir. Raises an execption on error.
-    testcell_name = '%s-%s-%d' % (platform, install_method, layout_index)
+def render(tmpdir, driver,cluster, platform, install_method):
+    """
+    Render the template in the tmpdir.
+    Raises an execption on error.
+    """
+    testcell_name = '%s-%s-%s' % (platform, install_method, cluster)
     template = pathlib.Path(__file__).parent.parent.parent
     cookiecutter(
         template=str(template),
@@ -76,14 +63,21 @@ def test_template(tmpdir, install_method, platform, layout_index):
         no_input=True,
         extra_context={
             'testcell_name': testcell_name,
-            'driver': 'vagrant/libvirt',
+            'driver': driver,
+            'cluster': cluster_types[cluster],
             'platform': platform,
             'install_method': install_method,
-            'install_with_dkms': ('yes' if install_with_dkms else 'no'),
-            'build_packages': ('yes' if build_packages else 'no'),
+            'install_with_dkms': 'yes',
+            'build_packages': 'yes',
         },
     )
     print("\nCreated scenario in directory %s/%s" % (tmpdir, testcell_name))
+    return testcell_name
+
+
+@pytest.mark.parametrize('driver,cluster,platform,install_method', params('render'))
+def test_render(tmpdir, driver,cluster, platform, install_method):
+    testcell_name = render(tmpdir, driver,cluster, platform, install_method)
 
     # Check generated files (except optional build tasks).
     files = [
@@ -108,7 +102,7 @@ def test_template(tmpdir, install_method, platform, layout_index):
                    'default' / 'molecule.yml').read_text()
     assert platform in molecule_yml
     assert install_method in molecule_yml
-    if do_build:
+    if install_method in ('packages', 'bdist', 'sdist'):
         assert 'afs_builders' in molecule_yml
     else:
         assert 'afs_builders' not in molecule_yml
@@ -116,34 +110,34 @@ def test_template(tmpdir, install_method, platform, layout_index):
     # Check the generated scenario file.
     prepare_yml = pathlib.Path(tmpdir / testcell_name / 'molecule' / \
                    'playbooks' / 'prepare.yml').read_text()
-    if do_build:
+    if install_method in ('packages', 'bdist', 'sdist'):
         assert 'import_playbook: build' in prepare_yml
 
-    # Optionally, check with yamllint and ansible-lint.
-    if lint:
-        with chdir(tmpdir / testcell_name):
-            rc = run_command('yamllint .')
-        assert rc == 0
+    # Check with yamllint and ansible-lint.
+    with chdir(tmpdir / testcell_name):
+        rc = run_command('yamllint .')
+    assert rc == 0
 
-        # Get our required collection.
-        with chdir(tmpdir / testcell_name):
-            rc = run_command('molecule dependency')
-        assert rc == 0
-        path = pathlib.Path.home() / '.cache' / 'molecule' / \
-               testcell_name / 'default' / 'collections'
-        cmd = 'ANSIBLE_COLLECTIONS_PATHS=%s ansible-lint' % (path)
+    # Get our required collection.
+    with chdir(tmpdir / testcell_name):
+        rc = run_command('molecule dependency')
+    assert rc == 0
+    path = pathlib.Path.home() / '.cache' / 'molecule' / \
+           testcell_name / 'default' / 'collections'
+    cmd = 'ANSIBLE_COLLECTIONS_PATHS=%s ansible-lint' % (path)
+    with chdir(tmpdir / testcell_name):
+        rc = run_command(cmd)
+    assert rc == 0
 
-        with chdir(tmpdir / testcell_name):
-            rc = run_command(cmd)
-        assert rc == 0
 
-    # Optionally, run the generated scenario. This can take a long time.
-    if run:
-        with chdir(tmpdir / testcell_name):
-            bcdir = pathlib.Path('~/.config/molecule').expanduser()
-            bc = bcdir / 'platforms' / ('%s.yml' % platform)
-            opt = (' --base-config=%s' % bc) if bc.exists() else ''
-            rc = run_command('molecule%s test' % opt)
-            assert rc == 0
-            rc = run_command('molecule%s reset' % opt)
-            assert rc == 0
+@pytest.mark.parametrize('driver,cluster,platform,install_method', params('run'))
+def test_run(tmpdir, driver,cluster, platform, install_method):
+    testcell_name = render(tmpdir, driver,cluster, platform, install_method)
+    with chdir(tmpdir / testcell_name):
+         bcdir = pathlib.Path('~/.config/molecule').expanduser()
+         bc = bcdir / 'platforms' / ('%s.yml' % platform)
+         opt = (' --base-config=%s' % bc) if bc.exists() else ''
+         rc = run_command('molecule%s test' % opt)
+         assert rc == 0
+         rc = run_command('molecule%s reset' % opt)
+         assert rc == 0
